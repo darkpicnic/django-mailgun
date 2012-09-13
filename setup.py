ae84784fb602a2779a254b4a216ccedb69e11847
Sebastian <darkpicnic@gmail.com>
2012-09-12 22:37:30 -0400

    WIP

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
+import sys
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-mailgun',
@@ -6,7 +11,9 @@ setup(
     packages=['mailgun'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description="A Django wrapper for the Mailgun API",
-    long_description=open('README.md').read(),
+    long_description=read('README'),
+    keywords='django mailgun',
+    author='Sebastian Lemery',
     author_email='darkpicnic@gmail.com',
     install_requires=[
            'requests >= 0.13.7',
