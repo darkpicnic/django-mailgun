0a94c25dcc5d0d38c58ac93d502d1960773afffb
Sebastian <darkpicnic@gmail.com>
2012-09-13 00:05:02 -0400

    WIP

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 import os
-import sys
-from setuptools import setup, find_packages
+from setuptools import setup
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='django-mailgun',
-    version='1.0.0',
+    name = "django-mailgun",
+    version = "1.0.0",
+    author = "Sebastian Lemery",
+    author_email = "darkpicnic@gmail.com",
+    description = "Django wrapper for Mailgun API",
+    license = "BSD",
+    keywords = "django mailgun",
+    url = "http://packages.python.org/",
     packages=['mailgun'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    description="A Django wrapper for the Mailgun API",
     long_description=read('README'),
-    keywords='django mailgun',
-    author='Sebastian Lemery',
-    author_email='darkpicnic@gmail.com',
     install_requires=[
-           'requests >= 0.13.7',
-        ],
+        'requests>=0.13.7'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
 )
